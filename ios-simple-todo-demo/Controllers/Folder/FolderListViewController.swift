f736a80e8018a9ca6d9eda6e0f50a43c1add3768
stv-ekushida <e.kushida@st-ventures.jp>
2017-06-01 10:50:37 +0900

    Add Update Folder

--- ios-simple-todo-demo/Controllers/Folder/FolderListViewController.swift
+++ ios-simple-todo-demo/Controllers/Folder/FolderListViewController.swift
@@ -13,11 +13,18 @@ final class FolderListViewController: UIViewController {
     @IBOutlet weak var tableView: UITableView!
     @IBOutlet weak var editButton: UIBarButtonItem!
     fileprivate var alert: UIAlertController!
+    fileprivate let dataSource = FolderListProvider()
 
     //MARK : - LifeCycle
     override func viewDidLoad() {
         super.viewDidLoad()
         setupNavigationBar()
+        setupTableView()
+    }
+
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        reloadFolderList()
     }
 
     //MARK : - Action
@@ -50,6 +57,14 @@ final class FolderListViewController: UIViewController {
             "すべて削除" : "新規作成"
     }
 
+    /// テーブルビューを設定する
+    private func setupTableView() {
+        dataSource.delegate = self
+        tableView.dataSource = dataSource
+        tableView.delegate = self
+        tableView.allowsSelectionDuringEditing = true
+    }
+
     /// フォルダの新規作成ダイアログを表示する
     private func showCreateFolderAlert() {
 
@@ -71,6 +86,12 @@ final class FolderListViewController: UIViewController {
             self?.alert = nil
         })
     }
+
+    /// フォルダ一覧を取得する
+    func reloadFolderList() {
+        dataSource.setFolders(folders: FolderDao.findAll())
+        tableView.reloadData()
+    }
 }
 
 //MARK : - AlertHelperDelegate
@@ -85,17 +106,63 @@ extension FolderListViewController: FolderAlertHelperDelegate {
 
         switch type {
         case .add:
-            print(folderName)
-            break
+            FolderDao.add(title: folderName)
 
         case .update(let index):
-            print(index)
-            break
+            let folder = dataSource.folder(index: index)
+            folder.title = folderName
+            FolderDao.update(folder: folder)
         }
+        reloadFolderList()
+        tableView.reloadData()
     }
 
     /// 全フォルダとそれに関連するメモを削除する
     func deleteAll() {
-        print(#function)
+        FolderDao.deleteAll()
+        reloadFolderList()
+        tableView.reloadData()
+    }
+}
+
+//MARK : - UITableViewDelegate
+extension FolderListViewController: UITableViewDelegate {
+
+    func tableView(_ tableView: UITableView,
+                   didSelectRowAt indexPath: IndexPath) {
+        tableView.deselectRow(at: indexPath, animated: true)
+
+        if isEditing {
+            showUpdateFolderAlert(row: indexPath.row)
+            return
+        }
+    }
+
+    /// フォルダ名の更新用ダイアログを表示する
+    ///
+    /// - Parameter row: TableViewの行番号
+    private func showUpdateFolderAlert(row: Int) {
+
+        let folder = dataSource.folder(index: row)
+
+        alert = FolderAlertHelper()
+            .createFolder(type: .update(index: row),
+                          title: "フォルダの名前を変更",
+                          message: "このフォルダの新しい名前を入力してください。",
+                          folderName: folder.title,
+                          delegate: self)
+
+        present(alert, animated: true, completion: { [weak self] in
+            self?.alert = nil
+        })
+    }
+}
+
+//MARK : - FolderProviderDelegate
+extension FolderListViewController: FolderListProviderDelegate {
+
+    func deleteFolder(index: Int) {
+        FolderDao.delete(folderID: index)
+        tableView.reloadData()
     }
 }
