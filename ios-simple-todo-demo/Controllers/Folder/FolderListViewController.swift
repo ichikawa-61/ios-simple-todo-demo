64bdb05cea7f8c45f07671308d55d51d52e71920
stv-ekushida <e.kushida@st-ventures.jp>
2017-06-01 10:24:37 +0900

    Add NavigationBar EditButton
    Add Toolbar EditButton

--- ios-simple-todo-demo/Controllers/Folder/FolderListViewController.swift
+++ ios-simple-todo-demo/Controllers/Folder/FolderListViewController.swift
@@ -10,8 +10,36 @@ import UIKit
 
 final class FolderListViewController: UIViewController {
 
+    @IBOutlet weak var tableView: UITableView!
+    @IBOutlet weak var editButton: UIBarButtonItem!
+
     //MARK : - LifeCycle
     override func viewDidLoad() {
         super.viewDidLoad()
+        setupNavigationBar()
+    }
+
+    //MARK : - Action
+    @IBAction func didTapEditFolder(_ sender: UIBarButtonItem) {
+    }
+
+    override func setEditing(_ editing: Bool, animated: Bool) {
+        super.setEditing(editing, animated: animated)
+        tableView.isEditing = editing
+        setupEditButton(isEditing: editing)
+    }
+
+    /// ナビゲーションバーを設定する
+    private func setupNavigationBar() {
+        navigationItem.rightBarButtonItem = editButtonItem
+        navigationItem.hideBackButtonTitle()
+    }
+
+    /// 編集ボタンを設定する
+    ///
+    /// - Parameter isEditing: 編集モードか？
+    private func setupEditButton(isEditing: Bool) {
+        editButton.title = isEditing ?
+            "すべて削除" : "新規作成"
     }
 }
