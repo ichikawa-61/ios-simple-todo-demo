76d6285dc0d10d2d0ef14d782265a00637878b5e
stv-ekushida <e.kushida@st-ventures.jp>
2017-06-01 11:34:55 +0900

    Add ToDo Model
    Add ToDoDAO

---
+++ ios-simple-todo-demo/Models/ToDo/ToDo.swift
@@ -0,0 +1,21 @@
+//
+//  ToDo.swift
+//  ios-simple-todo-demo
+//
+//  Created by Eiji Kushida on 2017/06/01.
+//  Copyright © 2017年 Eiji Kushida. All rights reserved.
+//
+
+import RealmSwift
+
+final class ToDo: Object {
+
+    dynamic var todoID = 0
+    dynamic var folderID = 0
+    dynamic var title = ""
+    dynamic var date: Date?
+
+    override static func primaryKey() -> String? {
+        return "todoID"
+    }
+}
---
+++ ios-simple-todo-demo/Models/ToDoDao.swift
@@ -0,0 +1,107 @@
+//
+//  ToDoDao.swift
+//  ios-simple-todo-demo
+//
+//  Created by Eiji Kushida on 2017/06/01.
+//  Copyright © 2017年 Eiji Kushida. All rights reserved.
+//
+
+import Foundation
+import RealmSwift
+
+final class ToDoDao {
+
+    static let dao = RealmDaoHelper<ToDo>()
+
+    /// ToDoを登録する
+    ///
+    /// - Parameters:
+    ///   - folderID: フォルダID
+    ///   - title:  タイトル
+    static func add(folderID: Int, title: String) {
+
+        let object = ToDo()
+        object.todoID = ToDoDao.dao.newId()!
+        object.folderID = folderID
+        object.title = title
+        object.date = Date().now()
+        ToDoDao.dao.add(data: object)
+    }
+
+    /// ToDoを更新する
+    ///
+    /// - Parameter todo: ToDo
+    static func update(todo: ToDo) {
+
+        guard let target = dao
+            .findFirst(key: todo.todoID as AnyObject) else {
+                return
+        }
+
+        let object = ToDo()
+        object.todoID = target.todoID
+        object.folderID = target.folderID
+        object.title = todo.title
+        object.date = Date().now()
+        _ = dao.update(data: object)
+    }
+
+    /// ToDoを削除する
+    ///
+    /// - Parameter todoID: ToDo-ID
+    static func delete(todoID: Int) {
+
+        guard let target = dao
+            .findFirst(key: todoID as AnyObject) else {
+                return
+        }
+        dao.delete(data: target)
+    }
+
+    /// フォルダ内のToDoをすべて削除する
+    ///
+    /// - Parameter folderID: フォルダID
+    static func deleteAll(folderID: Int) {
+        dao.deleteAll(key: "folderID" as AnyObject, value: folderID as AnyObject)
+    }
+
+    /// メモをすべて削除する
+    static func deleteAll() {
+        dao.deleteAll()
+    }
+
+    /// 該当のToDoを取得する
+    ///
+    /// - Parameter todoID: ToDo-ID
+    /// - Returns: ToDo
+    static func findByID(todoID: Int) -> ToDo? {
+        guard let object = dao
+            .findFirst(key: todoID as AnyObject) else {
+                return nil
+        }
+        return object
+    }
+
+    /// 該当のToDo一覧を取得する
+    ///
+    /// - Parameters:
+    ///   - key: 主キー
+    ///   - folderID: フォルダID
+    /// - Returns: ToDo一覧
+    static func findAll(key: String, folderID: Int) -> [ToDo] {
+        let objects = ToDoDao.dao
+            .findAll(key: key as AnyObject, value: folderID as AnyObject)
+            .sorted(byKeyPath: "date", ascending: false)
+        return objects.map { ToDo(value: $0) }
+    }
+
+    /// すべてのToDoを取得する
+    ///
+    /// - Returns: ToDo一覧
+    static func findAll() -> [ToDo] {
+        let objects = ToDoDao.dao
+            .findAll()
+            .sorted(byKeyPath: "date", ascending: false)
+        return objects.map { ToDo(value: $0) }
+    }
+}
